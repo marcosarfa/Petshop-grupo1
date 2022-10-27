a3309a8b52e78a9617be690cb9e6ed8183e83b61
marcosarfa <114246654+marcosarfa@users.noreply.github.com>
2022-10-27 20:27:08 -0300

    Add files via upload

---
+++ PETSHOP FINAL/assets/js/main.js
@@ -0,0 +1,40 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            urlApi:"https://apipetshop.herokuapp.com/api/articulos",
+            productos:[],
+            productosBk: [],
+            ultimosProductos:[],
+            buscador: ""
+        }
+    },
+    created(){
+        this.traerDatos()
+    },
+    methods:{
+        traerDatos(){
+            fetch(this.urlApi).then(response =>response.json())
+            .then(data =>{
+                this.productos= data.response
+                if(document.title=="Jugueteria"){
+                    this.productos=data.response.filter(producto => producto.tipo == "Juguete")
+                    this.ultimosProductos= this.productos.filter(producto => producto.stock<5)
+                } else if(document.title =="Farmacia"){
+                    this.productos=data.response.filter(producto => producto.tipo == "Medicamento")
+                    this.ultimosProductos= this.productos.filter(producto => producto.stock<5)
+                }
+                this.productosBk=this.productos
+            })
+
+        },
+        
+    },
+    computed:{ 
+        filtro(){
+            this.productos=this.productosBk.filter(producto => producto.nombre.toLowerCase().includes(this.buscador.toLowerCase())||producto.descripcion.toLowerCase().includes(this.buscador.toLowerCase()))
+        }
+        
+    }
+}).mount('#app')
